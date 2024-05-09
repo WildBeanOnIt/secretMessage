3f4506965781690a2987d9cbbfdfc4c6771f87bf
Julio Aguilera Jr <julioaguilera337@gmail.com>
2024-05-08 21:25:24 -0500

    Used .toSpliced to changed a word in a arr.

---
+++ main.js
@@ -0,0 +1,18 @@
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+//OEM Array haha
+console.log(secretMessage.length);
+// console.log(secretMessage);
+
+// New pop array.
+let removeLastString = secretMessage.pop();
+// New modified array
+console.log(secretMessage.length);
+
+// Adding "to" and "Program" to the array .push
+let addToAndProgramToArray = secretMessage.push("to", "Program");
+console.log(secretMessage);
+
+// Changing the word "easily" to "right" .toSpliced
+let changingEasilyToRight = secretMessage.toSpliced(7, 8, "right");
+console.log(secretMessage);
+console.log(changingEasilyToRight);
